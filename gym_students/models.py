e51d792a304d9126b44d514e241f5b6181e7582f
CodeBernardo <stein.bernardo@proton.me>
2024-04-15 10:55:49 -0300

    update

--- gym_students/models.py
+++ gym_students/models.py
@@ -1,12 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class GymStudent(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
+class GymStudent(AbstractUser):
     date_of_birth = models.DateField()
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=20)
     heigth = models.DecimalField(max_digits=3, decimal_places=2)
     weigth = models.DecimalField(max_digits=5, decimal_places=2)
 
